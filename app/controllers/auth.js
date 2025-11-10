4b0f9ba30fe63eb227a4f70551571279bf1b1f85
ARTEMIS1000 <krrishsinghmukesh07@gmail.com>
2025-11-09 22:45:47 -0500

    Update auth.js

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,6 +1,6 @@
 /* 
     File: auth.js
-    Developers: Salman Vahora, Bat An Dinh, Artemis, Edgar, Sriraj Bura
+    Developers: Salman Vahora, Bat An Dinh, Artemis | Krish Mukesh Singh, Edgar, Sriraj Bura
     Description: Handles user registration, login, and JWT authentication logic
     Date: November 09 2025
 */
